fec21f81d42192d2120df70612dc41dc920356d9
Umut Can Demir <umutcdemir@gmail.com>
2022-04-29 09:40:26 +0300

    .

---
+++ demo2/demo2/src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData=[
+  {
+    routeLink:'dashboard',
+    icon:'fal fa-home',
+    label:'Yönetim Paneli'
+  },
+  {
+    routeLink:'products',
+    icon:'far fa-file',
+    label:'CV'
+  },
+  {
+    routeLink:'statistics',
+    icon:'fas fa-users',
+    label:'Kullanıcılar'
+  },
+  {
+    routeLink:'coupens',
+    icon:'far fa-comments',
+    label:'Sohbet'
+  },
+  {
+    routeLink:'pages',
+    icon:'far fa-calendar-alt',
+    label:'Takvim'
+  },
+  {
+    routeLink:'media',
+    icon:'fas fa-cogs',
+    label:'Ayarlar'
+  },
+  {
+    routeLink:'settings',
+    icon:'fas fa-sign-out-alt',
+    label:'Çıkış Yap'
+  },
+];
